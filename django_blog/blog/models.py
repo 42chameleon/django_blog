6139cbfa5d60318288012bf696963c4fa2813c1f
Muhammad Israpilov <42israpilov@mail.ru>
2022-04-21 14:43:33 +0300

    created model Post and Comment

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL,
+                               blank=True, null=True,
+                               related_name='children')
+
+    class Meta:
+        verbose_name = "Комментария"
+        verbose_name_plural = "Комментарии"
